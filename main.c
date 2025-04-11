705badc27787e899ff0df0fa45bcc338fb724681
Hayden Freeman <haydenfreeman@yahoo.com>
2025-04-11 09:46:10 -0500

    ajskdlf

--- main.c
+++ main.c
@@ -3,30 +3,28 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
+#include <limits.h>
 #include <X11/Xlib.h>
 
-uint64_t scn, wdx, wdy, cx, cy, wth, hht, bwh, bdr, bgd; 
+int scn, wdx, wdy, cx, cy;
+unsigned int wth, hht, bwh;
+unsigned long bdr, bgd;
+
 Display* dpy;
 Window wdw, pnt;
 GC gc;
 
-int
-convInt(uint64_t i) {
-}
-
-void
-initDpy(void) {
+void initDpy(void) {
     dpy = XOpenDisplay(NULL);
     scn = DefaultScreen(dpy);
-    assert(scn <= INT16_MAX);
-    pnt = RootWindow(dpy, (int) scn);
+    pnt = RootWindow(dpy, scn);
     wdx = 10;
     wdy = 10;
     wth = 800;
     hht = 600;
     bwh = 1;
-    bdr = WhitePixel(dpy, (int) scn);
-    bgd = BlackPixel(dpy, (int) scn);
+    bdr = WhitePixel(dpy, scn);
+    bgd = BlackPixel(dpy, scn);
     wdw = XCreateSimpleWindow(dpy, pnt, wdx, wdy, wth, hht, bwh, bdr, bgd);
     XSelectInput(dpy, wdw, ExposureMask | KeyPressMask);
     XMapWindow(dpy, wdw);
@@ -34,24 +32,22 @@ initDpy(void) {
     XSetForeground(dpy, gc, WhitePixel(dpy, scn));
 }
 
-void
-drawInput(char c) {
+void drawInput(char c) {
     char str[2];
     if (cx == 0) { cx = 10; }
     if (cy == 0) { cy = 10; }
     str[0] = c;
     str[1] = '\0';
     XDrawString(dpy, wdw, gc, cx, cy, str, 1);
-    cx+=6;
-    if (cx >= 0 && (unsigned int) cx >= wth) { cx = 10; cy+=10; }
+    cx += 6;
+    if ((unsigned int)cx >= wth) { cx = 10; cy += 10; }
     XFlush(dpy);
 }
 
-char
-getInput(void) {
+char getInput(void) {
     XEvent e;
     KeySym key;
-    XNextEvent(dpy, &e);    
+    XNextEvent(dpy, &e);
     if (e.type == KeyPress) {
         key = XLookupKeysym(&e.xkey, 0);
         if (key <= 127) {
@@ -61,25 +57,22 @@ getInput(void) {
     return '\0';
 }
 
-void
-eventLoop(void) {
+void eventLoop(void) {
     while (1) {
-        char c;
-        c = getInput();
+        char c = getInput();
         drawInput(c);
     }
 }
 
-void
-closeDpy(void) {
+void closeDpy(void) {
     XDestroyWindow(dpy, wdw);
     XCloseDisplay(dpy);
 }
 
-int
-main(void) {
+int main(void) {
     initDpy();
     eventLoop();
     closeDpy();
     return 0;
 }
+
